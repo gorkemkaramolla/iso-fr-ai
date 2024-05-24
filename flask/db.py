417d094fca728ad4a16395ed6387e2d097431dff
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-05-24 13:33:07 +0300

    db functions are  commented for general network

--- flask/db.py
+++ flask/db.py
@@ -1,13 +1,13 @@
-import oracledb
-from config import DB_USER, DB_PASSWORD, DB_PORT, DB_SERVICE_NAME, DB_HOST
+# import oracledb
+# from config import DB_USER, DB_PASSWORD, DB_PORT, DB_SERVICE_NAME, DB_HOST
 
-connection = oracledb.connect(
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port=DB_PORT,
-    service_name=DB_SERVICE_NAME
-)
-cursor = connection.cursor()
-LOB = oracledb.LOB;
-ERROR = oracledb.Error;
+# connection = oracledb.connect(
+#     user=DB_USER,
+#     password=DB_PASSWORD,
+#     host=DB_HOST,
+#     port=DB_PORT,
+#     service_name=DB_SERVICE_NAME
+# )
+# cursor = connection.cursor()
+# LOB = oracledb.LOB;
+# ERROR = oracledb.Error;

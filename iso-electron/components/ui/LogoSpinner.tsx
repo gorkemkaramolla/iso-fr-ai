0af63e035952fd1992ca6146ea33253ed157d672
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-05-30 17:13:55 +0300

    Spinner Component For Loading

--- iso-electron/components/ui/LogoSpinner.tsx
+++ iso-electron/components/ui/LogoSpinner.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import Image from 'next/image';
 const RotatingWheel = () => {
   return (
-    <div className='relative'>
-      <img
+    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  '>
+      <Image
+        width={100}
+        height={100}
         src='/outer_wheel.png'
         alt='Outer Wheel'
         className='w-24 h-24 slow-animation'
       />
-      <img
+      <Image
+        width={100}
+        height={100}
         src='/inner_circle.png'
         alt='Inner Circle'
         className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[57.5px] h-[57.5px]'

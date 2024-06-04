2da8bf97d01d3d08cc05d2e7a0ea0713539c3863
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-06-04 15:01:54 +0300

    Added Jwt Features to the app.py

--- flask/app.py
+++ flask/app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 import os
-from routes import audio_bp
-from routes import camera_bp
-from routes import system_check
-
+from routes import audio_bp, camera_bp, system_check, auth_bp
+from flask_jwt_extended import JWTManager
 from socketio_instance import socketio
 
 app = Flask(__name__)
 CORS(app)
-
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') 
+jwt = JWTManager(app)
 app.register_blueprint(audio_bp)
 app.register_blueprint(camera_bp)
 app.register_blueprint(system_check)
+app.register_blueprint(auth_bp)
 
 os.makedirs("temp", exist_ok=True)
 os.makedirs("logs", exist_ok=True)

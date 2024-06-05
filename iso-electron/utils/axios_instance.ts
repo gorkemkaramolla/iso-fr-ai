120ca3d39d9379c2642447e2f5bdfcaafb6c9823
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-06-05 12:45:56 +0300

    chore: Refactor axios instance and add token refresh functionality

--- iso-electron/utils/axios_instance.ts
+++ iso-electron/utils/axios_instance.ts
@@ -1,15 +1,104 @@
 import axios from 'axios';
-import useStore from '@/lib/store';
+import { redirect } from 'next/navigation';
 
 function getAccessToken() {
-  const access_token = localStorage.getItem('access_token');
-  return access_token;
+  return sessionStorage.getItem('access_token') || '';
+}
+
+function getRefreshToken() {
+  return sessionStorage.getItem('refresh_token') || '';
 }
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_FLASK_URL,
+  headers: {
+    Authorization: `Bearer ${getAccessToken()}`,
+  },
 });
 
-api.defaults.headers.common['Authorization'] = `Bearer ${getAccessToken()}`;
+let isRefreshing = false;
+let failedQueue: {
+  resolve: (value: unknown) => void;
+  reject: (reason?: any) => void;
+}[] = [];
+
+const processQueue = (error: null, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers['Authorization'] = 'Bearer ' + token;
+            return api(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = getRefreshToken();
+
+      return new Promise((resolve, reject) => {
+        refreshAccessToken(refreshToken)
+          .then((newAccessToken) => {
+            sessionStorage.setItem('access_token', newAccessToken);
+            api.defaults.headers['Authorization'] = 'Bearer ' + newAccessToken;
+            originalRequest.headers['Authorization'] =
+              'Bearer ' + newAccessToken;
+            processQueue(null, newAccessToken);
+            resolve(api(originalRequest));
+          })
+          .catch((refreshError) => {
+            processQueue(refreshError, null);
+            redirect('/login'); // Redirect to login or handle session expiration
+            reject(refreshError);
+          })
+          .finally(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+async function refreshAccessToken(refreshToken: string) {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_FLASK_URL}/token/refresh`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${refreshToken}`, // Use refresh token here
+        },
+      }
+    );
+    console.log('Token refreshed:', response.data);
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error refreshing access token:', error);
+    throw error;
+  }
+}
 
 export default api;

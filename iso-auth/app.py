981dc9ca54fe8a0d619cc87442a780c8dffc4773
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-01 11:43:42 +0300

    Init ISO authorization backend and dockerfile

---
+++ iso-auth/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_cors import CORS
+import os
+from routes import (
+    auth_bp,
+    users_bp,
+)
+import flask.json.provider as provider
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
+app = Flask(__name__)
+provider.DefaultJSONProvider.sort_keys = False
+CORS(app, origins="*")
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(hours=2)
+jwt = JWTManager(app)
+app.register_blueprint(auth_bp)
+app.register_blueprint(users_bp)
+os.makedirs("logs", exist_ok=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
---
+++ iso-auth/auth/auth_provider.py
@@ -0,0 +1,38 @@
+from db import mongo_client, mongo_db
+from flask import jsonify, request
+from flask_jwt_extended import create_access_token, create_refresh_token,get_jwt_identity
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import timedelta
+class AuthProvider:
+    def __init__(self):
+        self.client = mongo_client
+        self.db = mongo_db
+    
+    def login(self, username, password):
+        users_collection = self.db["users"]
+        user = users_collection.find_one({"username": username})
+        if not user or not check_password_hash(user["password"], password):
+            return jsonify({"message": "Invalid credentials"}), 401
+
+        access_token = create_access_token(identity={"username": username})
+        refresh_token = create_refresh_token(identity={"username": username})
+        
+        return {"access_token": access_token, "refresh_token": refresh_token}
+    
+    def register(self, username, password):
+        users_collection = self.db["users"]
+        if users_collection.find_one({"username": username}):
+            return jsonify({"message": "Username already exists"}), 400
+        
+        hashed_password = generate_password_hash(password)
+        user_data = {
+            "username": username,
+            "password": hashed_password
+        }
+        users_collection.insert_one(user_data)
+        return jsonify({"message": "User registered successfully"}), 201
+    def refresh_token(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user)
+        return new_token
+    

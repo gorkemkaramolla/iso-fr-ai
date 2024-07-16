38e8c16077447d865d39779f1b554237066e42cf
Fatih Yavuz <yvzfth@yandex.com>
2024-07-16 09:53:31 +0300

    emotion model path update and disabled onnx warning messages

--- flask/emotion.py
+++ flask/emotion.py
@@ -1,64 +1,12 @@
-# import os
-# import requests
-# import onnxruntime as ort
-# import cv2
-# import numpy as np
-
-# # Step 1: Install Required Packages
-# # pip install onnxruntime numpy opencv-python
-
-# # Step 2: Download the ONNX Model
-
-
-# # Step 3: Load the Model
-# model_path = os.path.expanduser('~/.insightface/models/buffalo_l/onnx_model.onnx')
-# session = ort.InferenceSession(model_path)
-
-# # Step 4: Preprocess the Input Image
-# from PIL import Image
-
-# def preprocess_image(image_path):
-#   input_shape = (1, 1, 48, 48)
-#   img = Image.open(image_path)
-#   img = img.resize((48, 48), Image.Resampling.LANCZOS)
-#   img_data = np.array(img).astype(np.float32)  # Convert to float
-#   img_data /= 255.0  # Normalize to 0-1 if necessary
-#   img_data = np.resize(img_data, input_shape)
-#   return img_data
-
-# image_path = './face-images/happy.jpg'
-# input_image = preprocess_image(image_path)
-
-# # Step 5: Run Inference
-# input_name = session.get_inputs()[0].name
-# output_name = session.get_outputs()[0].name
-# result = session.run([output_name], {input_name: input_image})
-
-# emotion_scores = np.squeeze(result)
-
-# # Step 6: Interpret the Results
-# emotion_labels = [
-#     "neutral", "happiness", "surprise", "sadness",
-#     "anger", "disgust", "fear"
-# ]
-
-# emotion_dict = {emotion_labels[i]: float(emotion_scores[i]) for i in range(len(emotion_labels))}
-
-# # Get the emotion with the highest score
-# detected_emotion = max(emotion_dict, key=emotion_dict.get)
-
-# print(f"Detected emotion: {detected_emotion}")
-# print("Emotion scores:")
-# for emotion, score in emotion_dict.items():
-#     print(f"{emotion}: {score:.4f}")
 import os
 import onnx
 import onnxruntime
 import numpy as np
 from PIL import Image
 
+onnxruntime.set_default_logger_severity(3)  # Disable onnxruntime logging
 # Load the ONNX model
-model_path = os.path.expanduser('~/.insightface/models/buffalo_l/onnx_model.onnx')
+model_path = os.path.expanduser('~/.insightface/models/buffalo_l/emotion_model.onnx')
 session = onnxruntime.InferenceSession(model_path, providers=["CUDAExecutionProvider"])
 
 # Preprocess the input image
@@ -86,7 +34,7 @@ def postprocess(result):
     return predicted_emotion
 
 # Example usage
-image_path = './face-images/happy.jpeg'
+image_path = './face-images/angry1.jpg'
 result = predict(image_path)
 emotion = postprocess(result)
 print(f'Predicted Emotion: {emotion}')

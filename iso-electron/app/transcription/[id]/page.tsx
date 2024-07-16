c36011abf9a5eeecee51f5ed2dafa8305f9dab04
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-16 17:33:03 +0300

    Music Player added

--- iso-electron/app/transcription/[id]/page.tsx
+++ iso-electron/app/transcription/[id]/page.tsx
@@ -1,23 +1,16 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import TranscriptionHistory from '@/components/transcription/TranscriptionHistory';
-import Heading from '@/components/ui/Heading';
-import RotatingWheel from '@/components/ui/LogoSpinner';
-import useStore from '@/library/store';
-import api from '@/utils/axios_instance';
-import createApi from '@/utils/axios_instance';
 
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
+import React, { useEffect, useState, useRef } from 'react';
+import TranscriptionHistory from '@/components/transcription/TranscriptionHistory';
+import LogoSpinner from '@/components/ui/LogoSpinner';
+import createApi from '@/utils/axios_instance';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
-import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
-import LogoSpinner from '@/components/ui/LogoSpinner';
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
+import { Button } from 'primereact/button';
+import MusicPlayer from '@/components/sound/music-player';
+import useStore from '@/library/store';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 interface Segment {
   segment_id: string;
@@ -38,36 +31,44 @@ interface Props {
   };
 }
 
-interface Speaker {
-  id: string;
-  name: string;
-}
-
 const Transcription: React.FC<Props> = ({ params: { id } }) => {
   const [transcription, setTranscription] = useState<TranscriptionData | null>(
     null
   );
   const [showModal, setShowModal] = useState(false);
-  const [currentSpeaker, setCurrentSpeaker] = useState<Speaker>({
-    id: '',
-    name: '',
-  });
+  const [currentSpeaker, setCurrentSpeaker] = useState({ id: '', name: '' });
   const [newName, setNewName] = useState('');
+  const currentTime = useStore((state) => state.currentTime);
+  const transcriptionRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const getTranscription = async () => {
       try {
         const api = createApi(process.env.NEXT_PUBLIC_DIARIZE_URL);
-
         const response = await api.get(`/transcriptions/${String(id)}`);
-        console.log(response);
-        setTranscription(response.data);
+        if (response) setTranscription(response.data);
       } catch (error) {
         console.error('Error fetching transcription:', error);
       }
     };
     getTranscription();
   }, [id]);
+
+  useEffect(() => {
+    if (transcriptionRef.current) {
+      const segmentElements =
+        transcriptionRef.current.querySelectorAll('.segment');
+      segmentElements.forEach((element) => {
+        const start = parseFloat(
+          element.getAttribute('data-start-time') || '0'
+        );
+        const end = parseFloat(element.getAttribute('data-end-time') || '0');
+        if (currentTime >= start && currentTime <= end) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+      });
+    }
+  }, [currentTime]);
 
   const handleSpeakerClick = (segment: Segment) => {
     setCurrentSpeaker({ id: segment.segment_id, name: segment.speaker });
@@ -85,38 +86,88 @@ const Transcription: React.FC<Props> = ({ params: { id } }) => {
       setTranscription({ ...transcription!, segments: updatedSegments });
     }
     const api = createApi(process.env.NEXT_PUBLIC_DIARIZE_URL);
-
     await api.post(`/rename_segments/${id}/${oldName}/${newName}`, {});
   };
 
+  const getSpeakerColor = (speaker: string) => {
+    const colors = [
+      'bg-blue-100',
+      'bg-green-100',
+      'bg-yellow-100',
+      'bg-red-100',
+      'bg-purple-100',
+      'bg-pink-100',
+    ];
+    const hash = speaker
+      .split('')
+      .reduce((acc, char) => char.charCodeAt(0) + acc, 0);
+    return colors[hash % colors.length];
+  };
+
+  const handleGetExcel = () => {
+    if (transcription) {
+      const worksheet = XLSX.utils.json_to_sheet(transcription.segments);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Transcription');
+      const excelBuffer = XLSX.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      const data = new Blob([excelBuffer], {
+        type: 'application/octet-stream',
+      });
+      saveAs(data, `transcription_${id}.xlsx`);
+    }
+  };
+
+  const handleGetJSON = () => {
+    if (transcription) {
+      const json = JSON.stringify(transcription.segments, null, 2);
+      const blob = new Blob([json], { type: 'application/json' });
+      saveAs(blob, `transcription_${id}.json`);
+    }
+  };
+
   if (!transcription) {
-    return (
-      <div>
-        <LogoSpinner />
-      </div>
-    );
+    return <LogoSpinner />;
   }
 
   return (
-    <div className='flex flex-col'>
-      <h1 className='bold text-2xl'>{transcription.created_at}</h1>
-      <h2 className='bold text-xl'>Meclis Konuşması</h2>
-      <div className='flex items-start  w-full  justify-center'>
+    <div className='flex md:flex-row flex-col transition-all'>
+      <div className='flex flex-col p-4 w-full md:w-10/12 mx-auto'>
+        <div className='flex items-center justify-between'>
+          <MusicPlayer
+            audioSrc='/test.wav'
+            title='Meclis 1'
+            date={transcription.created_at}
+          />
+          <div className='mb-4 flex flex-col justify-end px-4 gap-2'>
+            <Button
+              label='Get Excel'
+              icon='pi pi-file-excel'
+              className='p-button-success w-full'
+              onClick={handleGetExcel}
+            />
+            <Button
+              label='Get JSON'
+              icon='pi pi-file'
+              className='p-button-info w-full'
+              onClick={handleGetJSON}
+            />
+          </div>
+        </div>
+
         <Dialog
           header='Rename Speaker'
           visible={showModal}
           onHide={() => setShowModal(false)}
-          style={{ width: '30vw' }}
+          style={{ width: '300px' }}
         >
           <div className='flex flex-col gap-2'>
-            <label htmlFor='newName' className='text-sm'>
-              New Name
-            </label>
             <InputText
-              id='newName'
               value={newName}
               onChange={(e) => setNewName(e.target.value)}
-              className='p-1 text-sm'
+              className='p-2 text-sm'
             />
             <div className='mt-4 flex justify-end'>
               <Button
@@ -133,48 +184,45 @@ const Transcription: React.FC<Props> = ({ params: { id } }) => {
             </div>
           </div>
         </Dialog>
+
         <div
-          className='overflow-x-auto w-10/12'
-          style={{ height: '650px', overflowY: 'scroll' }}
+          ref={transcriptionRef}
+          className='bg-gray-100 p-4 rounded-lg shadow-md overflow-y-auto'
+          style={{ maxHeight: '50vh' }}
         >
-          <div style={{ position: 'relative' }}>
-            <table className='table-auto w-full'>
-              <thead
-                style={{
-                  position: 'sticky',
-                  top: '0',
-                  backgroundColor: 'white',
-                  zIndex: 1,
-                }}
+          {transcription.segments.map((segment, index) => (
+            <div
+              key={segment.segment_id}
+              className={`mb-4 flex segment ${
+                index % 2 === 0 ? 'justify-start' : 'justify-end'
+              }`}
+              data-start-time={segment.start_time}
+              data-end-time={segment.end_time}
+            >
+              <div
+                className={`max-w-[70%] ${getSpeakerColor(
+                  segment.speaker
+                )} rounded-lg p-3 shadow`}
               >
-                <tr>
-                  <th className='px-4 py-2'>Video Zaman Aralığı</th>
-                  <th className='px-4 py-2'>Konuşmacı</th>
-                  <th className='px-4 py-2'>Transkript</th>
-                </tr>
-              </thead>
-              <tbody>
-                {transcription.segments.map((segment) => (
-                  <tr key={segment.segment_id}>
-                    <td className='border px-4 py-2'>
-                      {segment.start_time.toFixed(2)}s -{' '}
-                      {segment.end_time.toFixed(2)}s
-                    </td>
-                    <td
-                      className='border px-4 py-2 cursor-pointer hover:underline transition-all'
-                      onClick={() => handleSpeakerClick(segment)}
-                    >
-                      {segment.speaker}
-                    </td>
-                    <td className='border px-4 py-2'>
-                      {segment.transcribed_text}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                <div className='flex justify-between items-center mb-2'>
+                  <span
+                    className='font-bold cursor-pointer hover:underline'
+                    onClick={() => handleSpeakerClick(segment)}
+                  >
+                    {segment.speaker}
+                  </span>
+                  <span className='text-xs text-gray-500'>
+                    {segment.start_time.toFixed(2)}s -{' '}
+                    {segment.end_time.toFixed(2)}s
+                  </span>
+                </div>
+                <p>{segment.transcribed_text}</p>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
+      <div className='md:w-2/12 w-full'>
         <TranscriptionHistory />
       </div>
     </div>

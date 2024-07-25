0a51cc685291e1ab3bfc4542146c34eb55fb6eff
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-25 23:42:57 +0300

    chore: Changed Fluentd with Fluent Bit configuration for forwarding logs to Solr

---
+++ fluent-bit/config/transform.lua
@@ -0,0 +1,15 @@
+function modify_record(tag, timestamp, record)
+    if type(record["container_id"]) == "table" then
+        record["container_id"] = record["container_id"][1]
+    end
+    if type(record["container_name"]) == "table" then
+        record["container_name"] = record["container_name"][1]
+    end
+    if type(record["source"]) == "table" then
+        record["source"] = record["source"][1]
+    end
+    if type(record["log"]) == "table" then
+        record["log"] = record["log"][1]
+    end
+    return 1, timestamp, record
+end

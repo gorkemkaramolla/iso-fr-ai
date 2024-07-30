c392d8fb309c63b75e322717409c1048a0b6ed4c
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-30 21:55:11 +0300

    refactor: Update WhisperUpload component layout and add scroll functionality

--- iso-electron/app/page.tsx
+++ iso-electron/app/page.tsx
@@ -11,11 +11,11 @@ const Page: React.FC = () => {
   }, []);
   return (
     <div className='min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8'>
-      {/* <div className='container mx-auto'>
-        <ShowPersonel />
-      </div> */}
       <div className='max-w-3xl mx-auto mt-8'>
         <SearchComponent />
+      </div>
+      <div className='container mx-auto'>
+        <ShowPersonel />
       </div>
     </div>
   );

03861a2c093e72e788446faf58fa93c98d7d3fd1
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-30 21:54:43 +0300

    refactor: Update WhisperUpload component layout and add scroll functionality

--- iso-electron/app/monitoring/page.tsx
+++ iso-electron/app/monitoring/page.tsx
@@ -1,15 +1,21 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
-import { Cpu, MemoryStick, Box } from 'lucide-react';
-
-import { Expand, Copy, Check, Search } from 'lucide-react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
+import {
+  Cpu,
+  MemoryStick,
+  Box,
+  Expand,
+  Copy,
+  Check,
+  Search,
+} from 'lucide-react';
 import axios from 'axios';
 import Editor from '@monaco-editor/react';
-
 import useSystemInfo from '@/hooks/useSystemInfo';
 import GaugeWidget from '@/components/widgets/GaugeWidget';
 import UsageChart from '@/components/charts/usage';
 import Image from 'next/image';
+import { debounce } from 'lodash';
 
 const Dashboard: React.FC = () => {
   const { systemInfo, cpuUsageData, gpuUsageData } = useSystemInfo();
@@ -19,10 +25,44 @@ const Dashboard: React.FC = () => {
   const [searchResults, setSearchResults] = useState('');
   const [isSearching, setIsSearching] = useState(false);
   const [editorContent, setEditorContent] = useState('');
+  const [highlightTerm, setHighlightTerm] = useState('');
 
+  const containerRef = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<any>(null);
+  const triggerSearch = (searchText: string) => {
+    if (editorRef.current) {
+      const editor = editorRef.current;
+      const model = editor.getModel();
+      const matches = model.findMatches(
+        searchText,
+        true,
+        false,
+        false,
+        null,
+        true
+      );
+
+      // Highlight matches
+      if (matches.length > 0) {
+        editor.setSelections(
+          matches.map((match: any) => {
+            return {
+              selectionStartLineNumber: match.range.startLineNumber,
+              selectionStartColumn: match.range.startColumn,
+              positionLineNumber: match.range.endLineNumber,
+              positionColumn: match.range.endColumn,
+            };
+          })
+        );
+
+        // Scroll to the first match
+        editor.revealRange(matches[0].range);
+      }
+    }
+  };
   useEffect(() => {
-    console.log(systemInfo);
-  }, [systemInfo]);
+    setEditorContent(JSON.stringify(systemInfo.logs_data, null, 2));
+  }, [systemInfo.logs_data]);
 
   useEffect(() => {
     if (logCopied) {
@@ -31,20 +71,22 @@ const Dashboard: React.FC = () => {
   }, [logCopied]);
 
   useEffect(() => {
-    setEditorContent(JSON.stringify(systemInfo.logs_data, null, 2));
-  }, [systemInfo.logs_data]);
-
-  useEffect(() => {
     if (searchResults) {
       setEditorContent(searchResults);
+    } else {
+      setEditorContent(JSON.stringify(systemInfo.logs_data, null, 2));
     }
-  }, [searchResults]);
+  }, [searchResults, systemInfo.logs_data]);
 
-  const elementRef = useRef<HTMLDivElement>(null);
-
-  const goFullScreen = () => {
-    if (elementRef.current?.requestFullscreen) {
-      elementRef.current.requestFullscreen();
+  const toggleFullScreen = () => {
+    if (!document.fullscreenElement) {
+      if (containerRef.current?.requestFullscreen) {
+        containerRef.current.requestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
     }
   };
 
@@ -57,7 +99,7 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     setIsClient(true);
-    elementRef.current?.scrollTo(0, elementRef.current.scrollHeight);
+    containerRef.current?.scrollTo(0, containerRef.current.scrollHeight);
   }, []);
 
   const parseValue = (value: string) => {
@@ -65,17 +107,21 @@ const Dashboard: React.FC = () => {
     return isNaN(parsed) ? 0 : parsed;
   };
 
-  const handleSearch = async () => {
-    if (!searchQuery.trim()) return;
-
+  const performSearch = async (query: string) => {
     setIsSearching(true);
     try {
       const response = await axios.get(
         `${
           process.env.NEXT_PUBLIC_UTILS_URL
-        }/search_logs?query=${encodeURIComponent(searchQuery)}`
+        }/search_logs?query=${encodeURIComponent(query)}`
       );
+
       setSearchResults(JSON.stringify(response.data, null, 2));
+
+      console.log(searchQuery);
+      setTimeout(() => {
+        triggerSearch(searchQuery);
+      }, 1000);
     } catch (error) {
       console.error('Error searching logs:', error);
       setSearchResults('Error searching logs');
@@ -84,80 +130,89 @@ const Dashboard: React.FC = () => {
     }
   };
 
+  const debouncedSearch = useCallback(debounce(performSearch, 300), []);
+
+  useEffect(() => {
+    debouncedSearch(searchQuery);
+    return () => debouncedSearch.cancel();
+  }, [searchQuery, debouncedSearch]);
+
   const handleEditorDidMount = (editor: any, monaco: any) => {
     editor.updateOptions({ readOnly: true });
+    editorRef.current = editor;
   };
 
   if (!isClient) return null;
 
   return (
-    <div className='flex flex-col md:flex-row gap-4 p-4 bg-gray-100 min-h-screen'>
+    <div className='flex flex-col overflow-y-scroll md:flex-row gap-4 p-4 bg-gray-100 min-h-screen'>
       <div className='w-full container mx-auto space-y-4'>
         <div className='bg-white shadow-lg rounded-lg overflow-hidden'>
-          <div className='bg-gray-800 text-white p-4 flex flex-col sm:flex-row justify-between items-center'>
-            <h2 className='text-xl font-semibold mb-2 sm:mb-0'>System Logs</h2>
-            <div className='flex items-center space-x-2'>
-              <input
-                type='text'
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
-                placeholder='Search logs...'
-                className='px-2 py-1 rounded text-black w-40 sm:w-64'
-              />
-              <button
-                onClick={handleSearch}
-                disabled={isSearching}
-                className='p-2 rounded hover:bg-gray-700 transition-colors disabled:opacity-50'
-                title='Search'
-              >
-                <Search size={20} type='search' />
-              </button>
-              <button
-                onClick={handleLogCopy}
-                className='p-2 rounded hover:bg-gray-700 transition-colors'
-                title={logCopied ? 'Copied!' : 'Copy logs'}
-              >
-                {logCopied ? <Check size={20} /> : <Copy size={20} />}
-              </button>
-              <button
-                onClick={goFullScreen}
-                className='p-2 rounded hover:bg-gray-700 transition-colors'
-                title='Fullscreen'
-              >
-                <Expand size={20} />
-              </button>
+          <div ref={containerRef} className='flex flex-col h-full'>
+            <div className='bg-gray-800 text-white p-4 flex flex-col sm:flex-row justify-between items-center'>
+              <h2 className='text-xl font-semibold mb-2 sm:mb-0'>
+                Sistem Günlükleri
+              </h2>
+              <div className='flex items-center space-x-2'>
+                <input
+                  type='search'
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  placeholder='Sistem Günlüklerinde Ara...'
+                  className='px-2 py-1 rounded text-black w-40 sm:w-64'
+                />
+                <button
+                  onClick={handleLogCopy}
+                  className='p-2 rounded hover:bg-gray-700 transition-colors'
+                  title={logCopied ? 'Copied!' : 'Copy logs'}
+                >
+                  {logCopied ? <Check size={20} /> : <Copy size={20} />}
+                </button>
+                <button
+                  onClick={toggleFullScreen}
+                  className='p-2 rounded hover:bg-gray-700 transition-colors'
+                  title='Fullscreen'
+                >
+                  <Expand size={20} />
+                </button>
+              </div>
             </div>
-          </div>
-          <div ref={elementRef} className='min-h-96 h-96 overflow-hidden'>
-            <Editor
-              height='100%'
-              defaultLanguage='json'
-              value={editorContent}
-              theme={''}
-              options={{
-                minimap: { enabled: false },
-                scrollBeyondLastLine: false,
-                fontSize: 14,
-                readOnly: true,
-              }}
-              onMount={handleEditorDidMount}
-            />
+            <div className='flex-grow h-96'>
+              <Editor
+                height='100%'
+                defaultLanguage='json'
+                value={editorContent}
+                theme={''}
+                options={{
+                  minimap: { enabled: false },
+                  scrollBeyondLastLine: false,
+                  fontSize: 14,
+                  readOnly: true,
+                }}
+                onMount={handleEditorDidMount}
+              />
+            </div>
           </div>
         </div>
 
         <div className='flex flex-col md:flex-row gap-4'>
           <div className='w-full md:w-5/12 bg-white shadow-lg rounded-lg p-4'>
             <h2 className='text-xl font-semibold mb-4'>
-              CPU & GPU Temperatures
+              İşlemci & Ekran kartı Sıcaklıkları
             </h2>
             <div className='space-y-6'>
               <div>
-                <h3 className='text-lg font-medium mb-2'>CPU Temperature</h3>
-                <GaugeWidget value={Number(parseValue('99').toFixed(2))} />
+                <h3 className='text-lg font-medium mb-2'>İşlemci Sıcaklığı</h3>
+                <GaugeWidget
+                  value={Number(
+                    parseValue(systemInfo.host_cpu_temp).toFixed(2)
+                  )}
+                />
               </div>
               <div>
-                <h3 className='text-lg font-medium mb-2'>GPU Temperature</h3>
+                <h3 className='text-lg font-medium mb-2'>
+                  Ekran kartı Sıcaklığı
+                </h3>
                 <GaugeWidget
                   value={Number(
                     parseValue(systemInfo.host_gpu_temp).toFixed(2)
@@ -168,7 +223,7 @@ const Dashboard: React.FC = () => {
           </div>
           <div className='w-full md:w-7/12 bg-white shadow-lg rounded-lg p-4'>
             <h2 className='text-xl font-semibold mb-4'>
-              CPU and GPU Usage Chart
+              İşlemci & Ekran kartı Kullanım Grafiği
             </h2>
             <div className='h-64'>
               <UsageChart cpuData={cpuUsageData} gpuData={gpuUsageData} />
@@ -178,11 +233,10 @@ const Dashboard: React.FC = () => {
 
         <div className='bg-white shadow-lg rounded-lg p-4'>
           <h2 className='text-xl font-semibold mb-4 flex items-center'>
-            <Box className='mr-2' /> Container Information
+            <Box className='mr-2' /> Container Bilgileri
           </h2>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            {systemInfo.container_info.length === 0 ||
-            !systemInfo.container_info ? (
+            {systemInfo.container_info.length === 0 ? (
               <div>Loading</div>
             ) : (
               systemInfo.container_info.map((container, index) => (
@@ -196,18 +250,17 @@ const Dashboard: React.FC = () => {
                   </h3>
                   <p className='flex items-center mb-1'>
                     <Cpu className='mr-2' size={16} />
-                    CPU Usage:{' '}
+                    İşlemci Kullanımı:{' '}
                     <span className='font-semibold ml-1'>{container.cpu}</span>
                   </p>
                   <p className='flex items-center mb-1'>
                     <MemoryStick className='mr-2' size={16} />
-                    Memory Usage:{' '}
+                    Bellek Kullanımı:{' '}
                     <span className='font-semibold ml-1'>
                       {container.memory}
                     </span>
                   </p>
                   <p className='flex items-center'>
-                    {/* <Gpu className='mr-2' size={16} /> */}
                     <Image
                       width={900}
                       height={900}
@@ -215,7 +268,7 @@ const Dashboard: React.FC = () => {
                       className='mr-2 w-4 h-4'
                       src={'/nvidia.svg'}
                     />
-                    GPU Usage:{' '}
+                    Ekran Kartı Kullanımı:{' '}
                     <span className='font-semibold ml-1'>{container.gpu}</span>
                   </p>
                 </div>

8bd394e2b96af36f87ea3e23340ebf3d191aa3b2
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2024-07-30 21:54:28 +0300

    feat: Add framer motion animation to SettingsPage

--- iso-electron/app/settings/page.tsx
+++ iso-electron/app/settings/page.tsx
@@ -8,19 +8,26 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
 import 'primereact/resources/primereact.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
 import { Camera, User } from 'lucide-react';
+import { motion } from 'framer-motion';
+
 export default function SettingsPage() {
   return (
-    <div className='container mx-auto flex flex-col gap-4 h-[92vh] overflow-hidden p-4'>
-      <Panel header='Ayalar' className='h-full overflow-y-scroll'>
+    <motion.div
+      className='container mx-auto flex flex-col gap-4 h-[92vh] overflow-hidden p-4'
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.2 }}
+    >
+      <Panel header='Ayarlar' className='h-full overflow-y-scroll'>
         <div className='flex flex-col gap-4'>
           <Link href='/settings/camera_settings'>
             <Button
-              icon={<Camera className='h-5  w-5' />}
+              icon={<Camera className='h-5 w-5' />}
               label='Kamera Ayarları'
-              className='p-button-text p-button-plain gap-2  w-full text-left items-center flex '
+              className='p-button-text p-button-plain gap-2 w-full text-left items-center flex'
             />
           </Link>
-          <Link href='/settings/personel_settings' className=''>
+          <Link href='/settings/personel_settings'>
             <Button
               icon={<User className='h-5 w-5' />}
               label='Personel Ayarları'
@@ -29,6 +36,6 @@ export default function SettingsPage() {
           </Link>
         </div>
       </Panel>
-    </div>
+    </motion.div>
   );
 }

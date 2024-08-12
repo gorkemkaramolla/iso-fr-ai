c15972d757f7e44fd4d1359bfffc7bcb8c99b842
Fatih Yavuz <yvzfth@yandex.com>
2024-08-12 16:56:57 +0300

    refactor: Update providers for SCRFD model in camera_processor

--- iso-fr/services/camera_processor/scrfd.py
+++ iso-fr/services/camera_processor/scrfd.py
@@ -131,7 +131,7 @@ class SCRFD:
         if ctx_id < 0:
             self.session.set_providers(["CPUExecutionProvider"])
         else:
-            self.session.set_providers( "CUDAExecutionProvider")
+            self.session.set_providers( ["CUDAExecutionProvider"])
         nms_thresh = kwargs.get("nms_thresh", None)
         if nms_thresh is not None:
             self.nms_thresh = nms_thresh
